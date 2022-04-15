8ca9500eae5bb2c1908e8f5af154d88990c6bdc0
DevanHND <mohamadhenda@gmail.com>
2022-04-15 22:47:00 +0000

    fix: fix electron

--- electron-webpack.js
+++ electron-webpack.js
@@ -3,6 +3,6 @@ const { withExpoAdapter } = require('@expo/electron-adapter')
 
 module.exports = withExpoAdapter({
   projectRoot: __dirname,
-  whiteListedModules: ['recoil'],
+  whiteListedModules: ['recoil', 'formik'],
   // Provide any overrides for electron-webpack: https://github.com/electron-userland/electron-webpack/blob/master/docs/en/configuration.md
 })

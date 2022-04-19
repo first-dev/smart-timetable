7e54f3684604202fe29963ed3874b2890491bab5
DevanHND <mohamadhenda@gmail.com>
2022-04-19 01:39:21 +0000

    chore: cleanups

--- App.tsx
+++ App.tsx
@@ -3,13 +3,11 @@ import { Providers } from '@utils'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 
-/*
-TODO-Features:
-* ////dynamic timetable (changes based on current date)
-* swipe horizontally to navigate between weeks {TimetableView}
-
-
-TODO-Bugs:
+/* TODO-Features:
+ * ////dynamic timetable (changes based on current date)
+ * swipe horizontally to navigate between weeks {TimetableView}
+ */
+/* TODO-Bugs:
 ! can't access header buttons when header is not shown
 ! statusbar overlaps with content on landscape mode
 */

40b232ff09fbc3e42aa60a614f22a92895d131e7
DevanHND <mohamadhenda@gmail.com>
2022-05-01 16:34:04 +0000

    fix: fix style on web

--- src/components/UI/PlatformTouchable.tsx
+++ src/components/UI/PlatformTouchable.tsx
@@ -1,12 +1,14 @@
 import { ComponentProps, FC } from 'react'
-import { Platform } from 'react-native'
-import { TouchableNativeFeedback, TouchableOpacity } from 'react-native-gesture-handler'
-
-TouchableOpacity.defaultProps.activeOpacity = 0.8
+import { Platform, TouchableOpacity } from 'react-native'
+import { TouchableNativeFeedback } from 'react-native-gesture-handler'
 
 type Props = ComponentProps<typeof TouchableOpacity & typeof TouchableNativeFeedback>
 
-const PlatformTouchable: FC<Props> = (
-  Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
-) as any
+const PlatformTouchable: FC<Props> = props => {
+  return Platform.OS === 'android' ? (
+    <TouchableNativeFeedback {...props} />
+  ) : (
+    <TouchableOpacity activeOpacity={0.8} {...props} />
+  )
+}
 export default PlatformTouchable
